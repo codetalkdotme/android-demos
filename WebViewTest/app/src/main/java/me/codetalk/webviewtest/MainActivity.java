2c96c53518ac55b8309bc853e76739f068b880b5
guobxu <gbxu@outlook.com>
2017-12-15 01:05:59 +0800

    chnage

---
+++ WebViewTest/app/src/main/java/me/codetalk/webviewtest/MainActivity.java
@@ -0,0 +1,22 @@
+package me.codetalk.webviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // webview
+        WebView webView = findViewById(R.id.webview_1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.baidu.com");
+    }
+
+}
